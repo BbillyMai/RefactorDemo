16fcf13def470c46d45afc56c273c98b0ced48a4
bbillymai <1363543881@qq.com>
2019-08-19 21:30:53 +0800

    refactor updateQuality to switch methods

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -1,5 +1,10 @@
 package gildedRose;
+
 public class GildedRose {
+    private final String AGED_BRIE = "Aged Brie";
+    private final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    private final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -8,54 +13,62 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+            switch (items[i].name) {
+                case AGED_BRIE:
+                    items[i] = doAgedBrie(items[i]);
+                    break;
+                case BACKSTAGE_PASSES:
+                    items[i] = doBackstage(items[i]);
+                    break;
+                case SULFURAS:
+                    break;
+                default:
+                    items[i] = doElse(items[i]);
+                    break;
             }
         }
+    }
+
+    private Item doElse(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0 && item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+        return item;
+    }
+
+    private Item doBackstage(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+
+        if (item.sellIn < 11 && item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+
+        if (item.sellIn < 6 && item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+        return item;
+    }
+
+    private Item doAgedBrie(Item item) {
+        item.sellIn = item.sellIn - 1;
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+        if (item.sellIn < 0 && item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+        return item;
     }
 }
